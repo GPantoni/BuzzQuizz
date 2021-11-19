682c78ed2ec3dc1fbcb0ea9c6aaacb0fab087bf6
Ariel <ariel1242009@gmail.com>
2021-11-19 20:00:01 -0300

    implementação inicial das páginas

--- js/script.js
+++ js/script.js
@@ -1,24 +1,65 @@
+let objects = [];
+
 function loadQuizzList(){    
-    let promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+    const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
     promise.then(fillQuizz);
 
     const quizzesList = document.querySelector('.quizzes-list');
     const count = quizzesList.children.length; // = 5
-
+    let titlesCheck = [];
+    let idsCheck = [];
     function fillQuizz(reply){
+        objects = reply;
         for(let i = 0; i < reply.data.length; i++){
-            quizzesList.innerHTML += `
-            <li class="quizz">
-            <div class="quizz-card">
-            <h2>${reply.data[i].title}</h2>
-            </div>
-            </li>
-            `;
+            if(!titlesCheck.includes(reply.data[i].title)){
+                titlesCheck.push(reply.data[i].title);
+                idsCheck.push(i);
+                quizzesList.innerHTML += `
+                <li class="quizz" onclick="expandQuizz(this)">
+                <div class="quizz-card">
+                <h3>${reply.data[i].title}</h3>
+                </div>
+                </li>
+                `;
+            }
         }
         
         for(let j = count, i = 0; j < document.querySelector(".quizzes-list").children.length; j++, i++){
-            quizzesList.children[j].children[0].style.backgroundImage = `linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 65%, #000000 100%), url(${reply.data[i].image})`;
+            quizzesList.children[j].children[0].style.backgroundImage = `linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 65%, #000000 100%), url(${reply.data[idsCheck[i]].image})`;
         }
     }
 }
 loadQuizzList();
+
+function expandQuizz(element){
+    let title = element.children[0].children[0].innerHTML;
+    let tempObject = {};
+    for(let i = 0; i < objects.data.length; i++){
+        if(title === objects.data[i].title){
+            tempObject = objects.data[i];
+            break;
+        }
+    }
+    //Já temos o objeto no tempObject
+    //Resta exibir a página do quizz e esconder a anterior.
+    document.querySelector(".quizz-page").classList.remove("none");
+    document.querySelector(".initial-page").classList.add("none");
+}
+
+let elementAnswerQuizz = `
+    <article class="questions">
+        <div class="question">
+            <div class="question-title color0"><h2>|>>>>>|titulo_da_pergunta_aqui|<<<<<|</h2></div>
+
+            <ul class="answers">
+                <li class="answer">
+                    <div class="answer-image">|>>>>>|image_deve_ser_trocada_nos_estilos|<<<<<|</div>
+                    <div class="answer-text"><h3>|>>>>>|resposta_aqui}</h3></div>
+                </li>
+
+                ///// LI's de acordo com a quantidade de respostas do objeto
+
+            </ul>
+        </div>
+    </article>
+`;
