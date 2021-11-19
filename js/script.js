feed64d82b8ee04551ffffe35a9dcdc8244099a5
Ariel <ariel1242009@gmail.com>
2021-11-19 09:19:02 -0300

    fix da função de listagem

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 function loadQuizzList(){    
         let promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
         promise.then(fillQuizz);
+        const quizzesList = document.querySelector('.quizzes-list');
+        const count = quizzesList.children.length;
+
         function fillQuizz(reply){
-            const quizzesList = document.querySelector('.quizzes-list');
         const quizzesListUser = document.querySelector('.quizzes-list-user');
-        for(let i = 0; i < reply.data.length; i++){
+        for(let i = count; i < reply.data.length; i++){
             quizzesList.innerHTML += `
             <li class="quizz">
             <div class="quizz-card">
@@ -13,8 +15,8 @@ function loadQuizzList(){
             </li>
             `;
         }
-        for(let j = 0; j < document.querySelector(".quizzes-list").children.length; j++){
-            console.log(quizzesList.children[j].children[0])
+
+        for(let j = count; j < document.querySelector(".quizzes-list").children.length; j++){
             quizzesList.children[j].children[0].style.backgroundImage = `linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 65%, #000000 100%), url(${reply.data[j].image})`;
         }
     }
